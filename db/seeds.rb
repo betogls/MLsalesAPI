e4ad15326298eba2d3541e0eb07596002df139f9
betomaru <beto-gls@hotmail.com>
2016-03-25 08:36:22 +0000

    starting database configuration and test seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Product.create([{ url_image: "test/seed.png", catalog_price: 459.0, points: 30.0, product_line: "Moiskin" },
+                { url_image: "test2/seed.png", catalog_price: 400.0, points: 25.0, product_line: "Tolsom"},
+                { url_image: "test3/seed.png", catalog_price: 150.0, points: 8.0, product_line: "Nustrililte"}])
